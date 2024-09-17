075daf50792bf6f5b2fac2fdb80bbe5b391f1222
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2024-09-16 23:34:54 -0300

    chore: create endereco microsservice

---
+++ microsservice_endereco/src/main/java/com/ln/microsservice/endereco/Persistance/Entities/Endereco.java
@@ -0,0 +1,32 @@
+package com.ln.microsservice.endereco.Persistance.Entities;
+
+import com.ln.microsservice.endereco.Persistance.Entities.Enum.Estado;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer numero;
+    private String logradouro;
+    private String cidade;
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+    private Integer cep;
+}
